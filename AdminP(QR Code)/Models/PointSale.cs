711a555381093f4745e976803da39d8d1486d2ef
NiazbekovKuralbek <n.k.a.k.28241916@gmail.com>
2023-06-21 23:27:04 +0600

    Добавьте файлы проекта.

---
+++ AdminP(QR Code)/Models/PointSale.cs
@@ -0,0 +1,12 @@
+﻿namespace AdminP_QR_Code_.Models
+{
+    public class PointSale
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int State { get; set; }
+        public string qr_data { get; set; }
+       
+    }
+}
+
